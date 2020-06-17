71d6af3a773a5c17af8dd226d2ec93d210414977
Viktor Vinitski <fireworkv0@gmail.com>
2020-06-18 01:07:52 +0300

    commit

---
+++ changePicture.js
@@ -0,0 +1,15 @@
+function plus1() {
+    document.getElementById('pic').innerHTML += '<img src=одностворчатое.jpg style="margin-left: 70px;">'   
+}
+function plus2() {
+    document.getElementById('pic').innerHTML += '<img src=двустворчатое.jpg style="margin-left: 40px;">'    
+}
+function plus3() {
+    document.getElementById('pic').innerHTML += '<img src=трехстворчатое.jpg style="margin-left: 20px;">'        
+}
+function plus4() {
+    document.getElementById('pic').innerHTML += '<img src=балконный-блок.jpg>'        
+}
+function plus5() {
+    document.getElementById('pic').innerHTML += '<img src=остекление-балкона.jpg>'        
+}
